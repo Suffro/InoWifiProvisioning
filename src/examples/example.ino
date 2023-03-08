16c79b618704db9d916636612dd1da648858deb3
Lorenzo S <46894435+Suffro@users.noreply.github.com>
2023-03-08 16:33:22 +0100

    Update example.ino

--- src/examples/example.ino
+++ src/examples/example.ino
@@ -5,15 +5,18 @@ void setup() {
   // Initialize serial communication
   Serial.begin(9600);
   while (!Serial);
+  delay(100);
   Serial.println("Serial ready.");
   Serial.println();
   initProvisioningServer();
 }
 
 void loop() {
+  checkResetButton();
   handleProvisioningServer();
   if(WiFi.status() == WL_CONNECTED){
     // do your stuff
   }
 }
+
 
